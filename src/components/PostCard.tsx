5a2b65ab49b8efed112d0dc3b44d0a6599eb9973
Amar ghaly <amargad0112030@gmil.com>
2026-05-01 19:30:55 +0300

    feat(PostCart)profile style

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -111,7 +111,7 @@ export default function PostCard({ post }: { post: Post }) {
         space-y-4
       "
     >
-      <div className="flex items-center  justify-between">
+      <div className="flex items-center cursor-pointer  justify-between">
         <div
           className="flex gap-3"
           onClick={() => handleProfile(post.author?._id)}
